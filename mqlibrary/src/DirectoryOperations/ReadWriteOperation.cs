617c1b5290f459b6f4168f8e866e866637ec4457
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-02-29 21:16:36 +0500

    mqlibrary: added template for ReadWriteOperation

---
+++ mqlibrary/src/DirectoryOperations/ReadWriteOperation.cs
@@ -0,0 +1,18 @@
+namespace FileMqBroker.MqLibrary.ClientAdapters;
+
+public class ReadWriteOperation
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public void SaveRequestResponse(string method, string path, string response)
+    {
+        // string key = CalculateHash(method + path);
+        string key = $"{method}-{path}";
+        string requestFileName = key + ".req";
+        string responseFileName = key + ".resp";
+
+        File.WriteAllText(requestFileName, $"{method} {path}");
+        File.WriteAllText(responseFileName, $"200\n{response}");
+    }
+}
