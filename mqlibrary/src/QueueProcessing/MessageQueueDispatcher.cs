1dd0a6aebafadbfd8cdbae21e6eef4ed19099f97
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-03-03 20:17:27 +0500

    mqlibrary: template for queue dispatcher

--- mqlibrary/src/QueueProcessing/MessageQueueDispatcher.cs
+++ mqlibrary/src/QueueProcessing/MessageQueueDispatcher.cs
@@ -1,15 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FileMqBroker.MqLibrary.DAL;
+using FileMqBroker.MqLibrary.DirectoryOperations;
+using FileMqBroker.MqLibrary.Models;
+using FileMqBroker.MqLibrary.RuntimeQueues;
+
 namespace FileMqBroker.MqLibrary.QueueProcessing;
 
 /// <summary>
-/// 
+/// Performs a key mediator role within message queue processing.
 /// </summary>
 public class MessageQueueDispatcher
 {
-    // This class could be described as follows:
-    // - perform the function of a mediator: communicates directly in compliance with established levels:
-    //       - a class located at the DAL level (performs queries to the database),
-    //       - a class for working with directories,
-    //       - a class that contains a message queue for processing in memory.
-    // - runs on a timer.
-    // - plays a very important role in the application.
+    private readonly MessageFileDAL m_messageFileDAL;
+    private readonly FileHandler m_fileHandler;
+    private readonly MessageFileQueue m_messageFileQueue;
+
+    /// <summary>
+    /// Default constructor.
+    /// </summary>
+    public MessageQueueDispatcher(MessageFileDAL messageFileDAL, FileHandler fileHandler, MessageFileQueue messageFileQueue)
+    {
+        m_messageFileDAL = messageFileDAL;
+        m_fileHandler = fileHandler;
+        m_messageFileQueue = messageFileQueue;
+    }
+
+    /// <summary>
+    /// Method for processing the message queue from the producer side.
+    /// </summary>
+    public void ProcessProducer()
+    {
+        List<string> fileNames = m_messageFileQueue.DequeueMessages(10000); 
+
+        ThreadPool.QueueUserWorkItem(state =>
+        {
+            m_messageFileDAL.UpdateMessageFileState(fileNames, MessageFileState.Reading);
+        });
+
+        foreach (string fileName in fileNames)
+        {
+            ThreadPool.QueueUserWorkItem(state =>
+            {
+                ProcessFile(fileName);
+            });
+        }
+    }
+
+    /// <summary>
+    /// Performs processing of the specified file at the file creation level in the directory.
+    /// </summary>
+    private void ProcessFile(string fileName)
+    {
+        try
+        {
+            m_fileHandler.CreateFile(fileName);
+            m_fileHandler.WriteToFile(fileName, "Sample data");
+
+            Console.WriteLine($"File {fileName} processed successfully.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error processing file {fileName}: {ex.Message}");
+        }
+    }
 }
