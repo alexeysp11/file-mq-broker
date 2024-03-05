514655b9e5b11e7609063b2b0fc0d6510a963956
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-03-05 21:07:35 +0500

    mqlibrary: added method for inserting exceptions in dal

--- mqlibrary/src/DAL/ExceptionDAL.cs
+++ mqlibrary/src/DAL/ExceptionDAL.cs
@@ -12,6 +12,7 @@ namespace FileMqBroker.MqLibrary.DAL;
 public class ExceptionDAL
 {
     private readonly string m_connectionString;
+    private readonly string m_defaultInsertSQL = "INSERT INTO ExceptionLog (ExceptionMessage) VALUES ";
 
     /// <summary>
     /// Default constructor.
@@ -21,8 +22,40 @@ public class ExceptionDAL
         m_connectionString = appInitConfigs.DbConnectionString;
     }
 
-    public void InsertExceptions(List<string> exceptions)
+    /// <summary>
+    /// Method for inserting the specified exceptions into DB.
+    /// </summary>
+    public void InsertExceptions(IReadOnlyList<string> exceptions)
     {
-        // 
+        var sqlQuery = GenerateInsertSqlByExceptions(exceptions);
+
+        using (var connection = new SQLiteConnection(m_connectionString))
+        {
+            connection.Execute(sqlQuery.Query, sqlQuery.Parameters);
+        }
+    }
+
+    /// <summary>
+    /// Method for generating an SQL query for inserting data about exceptions.
+    /// </summary>
+    private (string Query, DynamicParameters Parameters) GenerateInsertSqlByExceptions(IReadOnlyList<string> exceptions)
+    {
+        if (exceptions == null)
+            throw new System.ArgumentNullException(nameof(exceptions));
+        
+        var queryParameters = new DynamicParameters();
+        var stringBuilder = new StringBuilder();
+        stringBuilder.Append(m_defaultInsertSQL);
+
+        for (int i = 0; i < exceptions.Count; i++)
+        {
+            if (i > 0)
+                stringBuilder.Append(", ");
+            stringBuilder.Append($"(@ExceptionMessage{i})");
+            
+            queryParameters.Add($"ExceptionMessage{i}", exceptions[i]);
+        }
+
+        return (stringBuilder.ToString(), queryParameters);
     }
 }
