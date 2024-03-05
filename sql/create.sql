4d2c53e7f6e3126637dacdb2008f097a48124e57
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-03-05 21:08:33 +0500

    sql: edited sql create

--- sql/create.sql
+++ sql/create.sql
@@ -1,24 +1,24 @@
 CREATE TABLE MessageFiles (
-    MessageFileId INTEGER PRIMARY KEY,
+    MessageFileId INTEGER PRIMARY KEY AUTOINCREMENT,
     Name TEXT NOT NULL,
     Content BLOB,
     ContentText TEXT,
     Size INTEGER,
-    CreatedAt DATETIME,
+    CreatedAt DATETIME DEFAULT CURRENT_TIMESTAMP,
     MessageFileState TEXT NOT NULL
 );
 
 CREATE TABLE FileStateHistory (
-    FileStateHistoryId INTEGER PRIMARY KEY,
+    FileStateHistoryId INTEGER PRIMARY KEY AUTOINCREMENT,
     MessageFileId INTEGER NOT NULL,
     NewState TEXT NOT NULL,
-    Timestamp DATETIME,
+    Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (MessageFileId) REFERENCES MessageFiles(MessageFileId)
 );
 
 CREATE TABLE ExceptionLog (
-    Id INTEGER PRIMARY KEY,
+    Id INTEGER PRIMARY KEY AUTOINCREMENT,
     ExceptionMessage TEXT NOT NULL,
-    StackTrace TEXT NOT NULL,
-    Timestamp DATETIME NOT NULL
+    StackTrace TEXT,
+    Timestamp DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
