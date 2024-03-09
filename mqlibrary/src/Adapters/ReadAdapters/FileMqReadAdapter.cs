922ae68da8feeeea34e799813f3c2f5a9c0ddedc
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-03-09 14:06:45 +0500

    mqlibrary: edited read adapters

--- mqlibrary/src/Adapters/ReadAdapters/FileMqReadAdapter.cs
+++ mqlibrary/src/Adapters/ReadAdapters/FileMqReadAdapter.cs
@@ -1,27 +1,43 @@
+using System.Threading;
 using FileMqBroker.MqLibrary.Models;
 using FileMqBroker.MqLibrary.RuntimeQueues;
 
 namespace FileMqBroker.MqLibrary.Adapters.ReadAdapters;
 
 /// <summary>
-/// 
+/// An adapter that is used on the client application side to read the message queue from the file broker.
 /// </summary>
 public class FileMqReadAdapter : IReadAdapter
 {
+    private int m_oneTimeProcQueueElements;
     private IReadMFQueue m_messageFileQueue;
+    private Action<MessageFile> m_continuationDelegate;
 
-    public FileMqReadAdapter(IReadMFQueue messageFileQueue)
+    /// <summary>
+    /// Default constructor.
+    /// </summary>
+    public FileMqReadAdapter(
+        AppInitConfigs appInitConfigs,
+        IReadMFQueue messageFileQueue,
+        Action<MessageFile> continuationDelegate)
     {
+        m_oneTimeProcQueueElements = appInitConfigs.OneTimeProcQueueElements;
         m_messageFileQueue = messageFileQueue;
+        m_continuationDelegate = continuationDelegate;
     }
     
-    public async Task<string> ReadMessageAsync()
+    /// <summary>
+    /// Method for reading messages from a queue.
+    /// </summary>
+    public void ReadMessageQueue()
     {
-        List<MessageFile> messages = m_messageFileQueue.DequeueMessages(1);
-        if (messages.Count > 0)
+        var messages = m_messageFileQueue.DequeueMessages(m_oneTimeProcQueueElements);
+        foreach (var message in messages)
         {
-            return messages[0].Content;
+            ThreadPool.QueueUserWorkItem(state =>
+            {
+                m_continuationDelegate(message);
+            });
         }
-        return string.Empty;
     }
 }
--- mqlibrary/src/Adapters/ReadAdapters/RabbitMqReadAdapter.cs
+++ mqlibrary/src/Adapters/ReadAdapters/RabbitMqReadAdapter.cs
@@ -3,19 +3,25 @@ using FileMqBroker.MqLibrary.RuntimeQueues;
 namespace FileMqBroker.MqLibrary.Adapters.ReadAdapters;
 
 /// <summary>
-/// 
+/// An adapter that is used on the client application side to read the message queue from RabbitMQ.
 /// </summary>
-public class RabbitMqReadAdapter : IReadAdapter
+public class RabbitMqReadAdapter<T> : IReadAdapter
 {
     private IReadMFQueue m_messageFileQueue;
 
+    /// <summary>
+    /// Default constructor.
+    /// </summary>
     public RabbitMqReadAdapter(IReadMFQueue messageFileQueue)
     {
         m_messageFileQueue = messageFileQueue;
     }
     
-    public async Task<string> ReadMessageAsync()
+    /// <summary>
+    /// Method for reading messages from a queue.
+    /// </summary>
+    public void ReadMessageQueue()
     {
-        return string.Empty;
+        // 
     }
 }
