a3194e6ad1d488dde5a27a0e80f594cc0b4f06b1
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-03-10 16:04:46 +0500

    backendservice: regester services

--- backendservice/src/Program.cs
+++ backendservice/src/Program.cs
@@ -1,9 +1,49 @@
+using FileMqBroker.MqLibrary.Adapters.ReadAdapters;
+using FileMqBroker.MqLibrary.Adapters.WriteAdapters;
 using FileMqBroker.MqLibrary.BackendService;
+using FileMqBroker.MqLibrary.BackendService.FileContentGenerators;
+using FileMqBroker.MqLibrary.KeyCalculations;
+using FileMqBroker.MqLibrary.KeyCalculations.FileNameGeneration;
+using FileMqBroker.MqLibrary.KeyCalculations.RequestCollapsing;
+using FileMqBroker.MqLibrary.Models;
+using FileMqBroker.MqLibrary.RuntimeQueues;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
-        services.AddHostedService<Worker>();
+        // App init configs.
+        services.AddSingleton(_ =>
+        {
+            return new AppInitConfigs
+            {
+                DbConnectionString = "",
+                RequestDirectoryName = "",
+                ResponseDirectoryName = "",
+                OneTimeProcQueueElements = 20_000,
+                DuplicateRequestCollapseType = DuplicateRequestCollapseType.Naive,
+                BackendContinuationDelegate = BackendServiceWorker.ContinuationMethod
+            };
+        });
+
+        // File content generation.
+        services.AddSingleton<MessageFileResponseGen>();
+
+        // Key calculations.
+        services.AddSingleton<KeyCalculationMD5>();
+        services.AddSingleton<KeyCalculationSHA256>();
+        services.AddSingleton<IFileNameGeneration, FileNameGenerationMD5>();
+        services.AddSingleton<IRequestCollapser, RequestCollapserSHA256>();
+
+        // Queues.
+        services.AddSingleton<IReadMFQueue, MessageFileQueue>();
+        services.AddSingleton<IWriteMFQueue, MessageFileQueue>();
+
+        // Queue adapters.
+        services.AddSingleton<IReadAdapter, FileMqReadAdapter>();
+        services.AddSingleton<IWriteAdapter, FileMqWriteAdapter>();
+
+        // Backend worker service.
+        services.AddHostedService<BackendServiceWorker>();
     })
     .Build();
 
