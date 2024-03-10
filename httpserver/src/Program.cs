9cd4932361ba6b5c8192f23668a5b5d08c93b76c
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-03-10 20:51:03 +0500

    httpserver: template for response handler

--- httpserver/src/Program.cs
+++ httpserver/src/Program.cs
@@ -1,3 +1,12 @@
+using FileMqBroker.HttpService.ResponseHandlers;
+using FileMqBroker.MqLibrary.Adapters.ReadAdapters;
+using FileMqBroker.MqLibrary.Adapters.WriteAdapters;
+using FileMqBroker.MqLibrary.KeyCalculations;
+using FileMqBroker.MqLibrary.KeyCalculations.FileNameGeneration;
+using FileMqBroker.MqLibrary.KeyCalculations.RequestCollapsing;
+using FileMqBroker.MqLibrary.Models;
+using FileMqBroker.MqLibrary.RuntimeQueues;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +15,37 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// App init configs.
+builder.Services.AddSingleton(_ =>
+{
+    return new AppInitConfigs
+    {
+        DbConnectionString = "",
+        RequestDirectoryName = "",
+        ResponseDirectoryName = "",
+        OneTimeProcQueueElements = 20_000,
+        DuplicateRequestCollapseType = DuplicateRequestCollapseType.Naive,
+        BackendContinuationDelegate = HttpResponseHandler.ContinuationMethod
+    };
+});
+
+// Key calculations.
+builder.Services.AddSingleton<KeyCalculationMD5>();
+builder.Services.AddSingleton<KeyCalculationSHA256>();
+builder.Services.AddSingleton<IFileNameGeneration, FileNameGenerationMD5>();
+builder.Services.AddSingleton<IRequestCollapser, RequestCollapserSHA256>();
+
+// Queues.
+builder.Services.AddSingleton<IReadMFQueue, MessageFileQueue>();
+builder.Services.AddSingleton<IWriteMFQueue, MessageFileQueue>();
+
+// Queue adapters.
+builder.Services.AddSingleton<IReadAdapter, FileMqReadAdapter>();
+builder.Services.AddSingleton<IWriteAdapter, FileMqWriteAdapter>();
+
+// Backend worker service.
+builder.Services.AddHostedService<HttpResponseHandler>();
 
 var app = builder.Build();
 
---
+++ httpserver/src/ResponseHandlers/HttpResponseHandler.cs
@@ -0,0 +1,50 @@
+using FileMqBroker.MqLibrary.Adapters.ReadAdapters;
+using FileMqBroker.MqLibrary.Models;
+
+namespace FileMqBroker.HttpService.ResponseHandlers;
+
+/// <summary>
+/// Processes responses from the backend and sends them to the client application via HTTP.
+/// </summary>
+public class HttpResponseHandler : BackgroundService
+{
+    private readonly ILogger<HttpResponseHandler> m_logger;
+    private IReadAdapter m_readAdapter;
+
+    /// <summary>
+    /// Default constructor.
+    /// </summary>
+    public HttpResponseHandler(
+        ILogger<HttpResponseHandler> logger,
+        IReadAdapter readAdapter)
+    {
+        m_logger = logger;
+        m_readAdapter = readAdapter;
+    }
+
+    /// <summary>
+    /// Method for executing worker functionality.
+    /// </summary>
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            m_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            m_readAdapter.ReadMessageQueue();
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+    
+    /// <summary>
+    /// Method that is used to process a message received from a message broker.
+    /// </summary>
+    public static void ContinuationMethod(MessageFile messageFile)
+    {
+        if (messageFile == null)
+            throw new System.ArgumentNullException(nameof(messageFile));
+
+        System.Console.WriteLine("Message file name: {messageFileName}, {content}", messageFile.Name, messageFile.Content);
+
+        // Send the message via HTTP.
+    }
+}
