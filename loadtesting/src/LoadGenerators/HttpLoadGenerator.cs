d2d319efba67c095a921e531a1cb52624bd4fae1
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-03-12 23:30:48 +0500

    loadtesting: edited description for load generators

--- loadtesting/src/LoadGenerators/HttpLoadGenerator.cs
+++ loadtesting/src/LoadGenerators/HttpLoadGenerator.cs
@@ -3,14 +3,14 @@ using FileMqBroker.MqLibrary.LoadTesting.LoadCalculations;
 namespace FileMqBroker.MqLibrary.LoadTesting.LoadGenerators;
 
 /// <summary>
-/// 
+/// Load generator that calls the adapter for the message broker over HTTP.
 /// </summary>
 public class HttpLoadGenerator : ILoadGenerator
 {
     private ILoadCalculation m_loadCalculation;
 
     /// <summary>
-    /// 
+    /// Default constructor.
     /// </summary>
     public HttpLoadGenerator(ILoadCalculation loadCalculation)
     {
@@ -18,7 +18,7 @@ public class HttpLoadGenerator : ILoadGenerator
     }
 
     /// <summary>
-    /// 
+    /// Method that generates a load for the selected generator type.
     /// </summary>
     public void GenerateLoad()
     {
--- loadtesting/src/LoadGenerators/LibraryLoadGenerator.cs
+++ loadtesting/src/LoadGenerators/LibraryLoadGenerator.cs
@@ -3,14 +3,14 @@ using FileMqBroker.MqLibrary.LoadTesting.LoadCalculations;
 namespace FileMqBroker.MqLibrary.LoadTesting.LoadGenerators;
 
 /// <summary>
-/// 
+/// Load generator that calls the message broker adapter as a library component.
 /// </summary>
 public class LibraryLoadGenerator : ILoadGenerator
 {
     private ILoadCalculation m_loadCalculation;
 
     /// <summary>
-    /// 
+    /// Default constructor.
     /// </summary>
     public LibraryLoadGenerator(ILoadCalculation loadCalculation)
     {
@@ -18,15 +18,15 @@ public class LibraryLoadGenerator : ILoadGenerator
     }
 
     /// <summary>
-    /// 
+    /// Method that generates a load for the selected generator type.
     /// </summary>
     public void GenerateLoad()
     {
         var currentLoad = m_loadCalculation.CalculateLoad();
         System.Console.WriteLine($"Current load: {currentLoad}");
 
-        // Нагрузка означает количество реквестов.
-        // Соответственно, необходимо в цикле создать заданное количество клиентских классов.
+        // Load means the number of requests.
+        // Accordingly, it is necessary to create a given number of client classes in a loop.
         for (int i = 0; i < currentLoad; i++)
         {
             // 
