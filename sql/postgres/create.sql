9752811aeec34c960272f7beb897ab563e638f94
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-03-16 22:43:05 +0500

    sql: added sql for postgres

---
+++ sql/postgres/create.sql
@@ -0,0 +1,34 @@
+CREATE TABLE RequestMessageFiles (
+    Id SERIAL PRIMARY KEY,
+    Name TEXT NOT NULL,
+    HttpMethod TEXT,
+    HttpPath TEXT,
+    Size INTEGER,
+    CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    MessageFileState INTEGER NOT NULL
+);
+
+CREATE TABLE FileStateHistory (
+    Id SERIAL PRIMARY KEY,
+    RequestMessageFileId INTEGER NOT NULL,
+    NewState TEXT NOT NULL,
+    Timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (RequestMessageFileId) REFERENCES RequestMessageFiles(Id)
+);
+
+CREATE TABLE ResponseMessageFiles (
+    Id SERIAL PRIMARY KEY,
+    Name TEXT NOT NULL,
+    HttpMethod TEXT,
+    HttpPath TEXT,
+    Size INTEGER,
+    CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    MessageFileState INTEGER NOT NULL
+);
+
+CREATE TABLE ExceptionLog (
+    Id SERIAL PRIMARY KEY,
+    ExceptionMessage TEXT NOT NULL,
+    StackTrace TEXT,
+    Timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
